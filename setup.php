3ff69521c5b0aded290969d904b8913d7ff00caf
koryo <koryovip@yahoo.co.jp>
2016-10-10 18:59:48 +0900

    setup.php

---
+++ setup.php
@@ -0,0 +1,8 @@
+<?php
+	// アプリケーションID, シークレッvト
+	$client_id = "your_client_id";
+	$client_secret = "your_client_secret";
+	$redirect_uri = "http://127.0.0.1";
+	$state = 'xxxxxxxxxxxxxxxxxxxxxxx';
+
+	$G_SELLER_ID = 'your_seller_id';
