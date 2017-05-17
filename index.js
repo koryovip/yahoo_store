9efe509d2a110c3b9c277e7d07b098a0d465a121
koryo <koryovip@yahoo.co.jp>
2017-05-17 23:14:06 +0900

    test

--- index.js
+++ index.js
@@ -3,22 +3,18 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const request = require('request');
 const cheerio = require('cheerio');
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database(':memory:'); // https://github.com/mapbox/node-sqlite3
 
 const headers = {
-  'Content-Type': 'text/html; charset=UTF-8'
-}
+	'Content-Type': 'text/html; charset=UTF-8'
+};
 
 const options = {
 	url: 'https://www.mercari.com/jp/search/?keyword=%E3%83%9F%E3%82%AD%E3%83%8F%E3%82%A6%E3%82%B9',
 	headers: headers,
 	json: false
 };
-
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database(':memory:');
-db.serialize(function() {
-  db.run("CREATE TABLE items (url TEXT PRIMARY KEY NOT NULL, img TEXT NOT NULL, name TEXT NOT NULL, price TEXT NOT NULL)");
-});
 
 var getItemList = function(callback) {
 	var items_new = [];
@@ -56,9 +52,9 @@ var insertData = function(stmt1, stmt2, item_new) {
 				resolve(null);
 				return;
 			}
-			//console.log('1');
+			// console.log('1');
 			stmt2.run(item_new.url, item_new.img, item_new.name, item_new.price);
-			//console.log('2');
+			// console.log('2');
 			resolve(item_new);
 		});
 	});
@@ -78,7 +74,7 @@ var diffItemList = function() {
 				Promise.all(promises).then(function(results) {
 					stmt1.finalize();
 					stmt2.finalize();
-					db.get('SELECT count(*) cnt FROM items', function(err, row){
+					db.get('SELECT count(*) cnt FROM items', function(err, row) {
 						console.log('db row count:' + row.cnt);
 						var diff = [];
 						results.forEach(function (result, index, results) {
@@ -99,56 +95,71 @@ var diffItemList = function() {
 	}, 3000);
 };
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+app.get('/js/:name', function (req, res, next) {
+  var options = {
+    root: __dirname + '/js/',
+    dotfiles: 'deny',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+  };
+  var fileName = req.params.name;
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      next(err);
+    } else {
+      console.log('Sent:', fileName);
+    }
+  });
+});
+
+io.on('connection', function(socket) {
 	console.log('a user connected');
-	socket.on('disconnect', function(){
+	socket.on('disconnect', function() {
 		console.log('user disconnected');
 		// db.close();
 	});
-
 	db.serialize(function() {
-		db.get('SELECT count(*) cnt FROM items', function(err, row) {
-			console.log('db row count:' + row.cnt);
-			if (row.cnt > 0) {
-				// has data
-				var items = [];
-				db.get('select * from items', function(err, row) {
-					items.push(row);
-				});
-				io.emit('event_init', items);
-			} else {
-				getItemList(function(items_new) {
-					db.serialize(function() {
-						// db.run('CREATE TABLE items (url TEXT PRIMARY KEY NOT NULL, img TEXT NOT NULL, name TEXT NOT NULL, price TEXT NOT NULL)');
-						var stmt1 = db.prepare('SELECT count(*) cnt FROM items where url=?');
-						var stmt2 = db.prepare('INSERT INTO items VALUES (?,?,?,?)');
-						var promises = [];
-						items_new.forEach(function (item_new, index_new, array_new) {
-							promises.push(insertData(stmt1, stmt2, item_new));
-						});
-						Promise.all(promises).then(function(results) {
-							// results is not use
-							stmt1.finalize();
-							stmt2.finalize();
-							db.get('SELECT count(*) cnt FROM items', function(err, row){
-								console.log('db row count:' + row.cnt);
-								io.emit('event_init', results);
-								diffItemList();
-							});
-						}).catch(function() {
-							console.log('error');
-						});
-					});
-				});
-			}
+		var items = [];
+		db.each('select * from items', function(err, row) {
+			items.push(row);
+		}, function() {
+			// complete
+			console.log(items.length);
+			io.emit('event_init', items);
 		});
 	});
 });
 
-http.listen(3000, function(){
+http.listen(3000, function() {
 	console.log('listening on *:3000');
+	db.serialize(function() {
+		// create db in memory
+		db.run("CREATE TABLE items (url TEXT PRIMARY KEY NOT NULL, img TEXT NOT NULL, name TEXT NOT NULL, price TEXT NOT NULL)");
+		// get items first time. init
+		getItemList(function(items_new) {
+			var stmt1 = db.prepare('SELECT count(*) cnt FROM items where url=?');
+			var stmt2 = db.prepare('INSERT INTO items VALUES (?,?,?,?)');
+			var promises = [];
+			items_new.forEach(function (item_new, index_new, array_new) {
+				promises.push(insertData(stmt1, stmt2, item_new));
+			});
+			Promise.all(promises).then(function(results) {
+				// console.log(results.length);
+				stmt1.finalize();
+				stmt2.finalize();
+				db.get('SELECT count(*) cnt FROM items', function(err, row) {
+					console.log('db row count:' + row.cnt);
+				});
+				diffItemList();
+			}).catch(function() {
+				console.log('error');
+			});
+		});
+	});
 });
